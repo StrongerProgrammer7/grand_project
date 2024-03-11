8e82e272df0ade67791af03df84de3a2e8827ea4
Abdyukov Zufar <swat55551@gmail.com>
2024-03-11 15:04:17 +0300

    Added API booking table and added description to swagger

--- web/models/Api/POST/book_table.js
+++ web/models/Api/POST/book_table.js
@@ -1,6 +1,8 @@
 const ApiError = require("../../../HandleAPI/ApiError");
 const DataApi = require("../../../HandleAPI/DataApi");
+const errorHandler = require('./errorHandler');
 const db = require('../../db');
+
 const book_table = async (req, res, next) =>
 {
     if (!req.body)
@@ -8,16 +10,48 @@ const book_table = async (req, res, next) =>
     const
         {
             id_table,
-            order_time,
+            id_worker,
             phone_client,
+            order_time,
             desired_booking_time
         } = req.body;
-    if (!(id_table && order_time && phone_client && desired_booking_time))
+    if (!(id_table && id_worker && order_time && phone_client && desired_booking_time))
         return next(ApiError.badRequest("Don't enought data!"));
+    let booking_interval = calcDiffData(order_time, desired_booking_time) + " days";
+    db.query('CALL book_table($1,$2,$3,$4,$5,$6)', [
+        id_table,
+        id_worker,
+        phone_client,
+        order_time,
+        desired_booking_time,
+        booking_interval
+    ])
+        .then(() =>
+        {
+            return next(DataApi.success({}, "Table is book successfully!"));
+        })
+        .catch(err =>
+        {
+            errorHandler(
+                "Error with book table",
+                ["23505"],
+                "Table is already book or don't exists table/worker, check your data",
+                "Internal error with booking table!",
+                err,
+                next
+            );
+        });
 
-    /*const result = await db.proc('book_table', []);*/
 
-    return next(DataApi.success({}, "Request execution"));
+}
+
+function calcDiffData(order_time, desired_booking_time)
+{
+    var date1 = new Date(order_time);
+    var date2 = new Date(desired_booking_time);
+    var timeDiff = Math.abs(date2.getTime() - date1.getTime());
+    var diffDays = Math.ceil(timeDiff / (1000 * 3600 * 24));
+    return diffDays;
 }
 
 module.exports = book_table;

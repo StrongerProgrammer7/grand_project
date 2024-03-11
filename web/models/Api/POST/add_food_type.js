fcac9816fb6ef32809e7786e28fc0dea3c0911f3
Abdyukov Zufar <swat55551@gmail.com>
2024-03-11 13:01:02 +0300

    Ready API: add food type and add ingridient and added description API in Swagger docs

--- web/models/Api/POST/add_food_type.js
+++ web/models/Api/POST/add_food_type.js
@@ -1,5 +1,6 @@
 const ApiError = require("../../../HandleAPI/ApiError");
 const DataApi = require("../../../HandleAPI/DataApi");
+const errorHandler = require('./errorHandler');
 const db = require('../../db');
 const add_food_type = async (req, res, next) =>
 {
@@ -12,9 +13,24 @@ const add_food_type = async (req, res, next) =>
     if (!type)
         return next(ApiError.badRequest("Don't enought data!"));
 
-    /*const result = await db.proc('add_food_type', []);*/
+    db.query('CALL add_food_type($1)', [type])
+        .then(() =>
+        {
+            return next(DataApi.success({}, "Type of food added!"));
+        })
+        .catch(err =>
+        {
+            errorHandler(
+                "Error with add type of food",
+                "23505",
+                "Type of food is exists, check your data",
+                "Internal error with add type of food!",
+                err,
+                next
+            );
+        })
 
-    return next(DataApi.success({}, "Request execution"));
+
 }
 
 module.exports = add_food_type;
--- web/models/Api/POST/add_ingridient.js
+++ web/models/Api/POST/add_ingridient.js
@@ -1,27 +1,60 @@
 const ApiError = require("../../../HandleAPI/ApiError");
 const DataApi = require("../../../HandleAPI/DataApi");
+const errorHandler = require('./errorHandler');
 const db = require('../../db');
+
 const add_ingridient = async (req, res, next) =>
 {
     if (!req.body)
         return next(ApiError.badRequest("Request body is empty!"));
+    let update = false;
     const
         {
             name,
             measurement,
             critical_rate,
             price,
-            update // true/false
         } = req.body;
+    if (req.body.update)
+        update = req.body.update;
     if (!(name && measurement && critical_rate && price))
         return next(ApiError.badRequest("Don't enought data!"));
 
-    if (update === true)
-        console.log();/*const result = await db.proc('add_ingredient', []);*/
+    if (update === false)
+        return addIngredient({ name, measurement, critical_rate, price }, next);
     else
         console.log();/*const result = await db.proc('update_ingredient', []);*/
 
-    return next(DataApi.success({}, "Request execution"));
+
 }
 
+function updateIngrdient()
+{
+    return next(DataApi.success({}, "Ingredient update!"));
+}
+
+function addIngredient(data, next)
+{
+    db.query('CALL add_ingredient($1,$2,$3,$4)', [
+        data.name,
+        data.measurement,
+        data.critical_rate,
+        data.price
+    ])
+        .then(() =>
+        {
+            return next(DataApi.success({}, "Ingredient added!"));
+        })
+        .catch(err =>
+        {
+            errorHandler(
+                "Error with add ingredient",
+                "23505",
+                "ingredient is exists, check your data",
+                "Internal error with add ingredient!",
+                err,
+                next
+            );
+        });
+}
 module.exports = add_ingridient;

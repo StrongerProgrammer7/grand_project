de5b4d53d775174704c0f57564843ab6bb8993de
Abdyukov Zufar <swat55551@gmail.com>
2024-03-17 13:35:25 +0300

    Api: Added: - update salary and rate of worker

--- web/controller/controller.js
+++ web/controller/controller.js
@@ -23,6 +23,7 @@ const get_all_tables_on_date = require('../models/Api/POST/get_all_tables_on_dat
 const get_booked_tables_on_date = require('../models/Api/POST/get_booked_tables_on_date');
 const add_order_ingredient = require('../models/Api/POST/add_order_ingredient');
 const update_order = require('../models/Api/POST/update_order');
+const update_worker_salary_and_rate = require('../models/Api/POST/update_worker_salary_and_rate');
 
 router.post('/add_food', add_food);
 router.post('/add_food_composition', add_food_composition);
@@ -44,7 +45,7 @@ router.post('/get_booked_tables_on_date', get_booked_tables_on_date);
 router.post('/get_all_tables_on_date', get_all_tables_on_date);
 router.post('/add_order_ingredient', add_order_ingredient);
 router.post('/update_order', update_order);
-
+router.post('/update_worker_salary_and_rate', update_worker_salary_and_rate);
 //DELETE
 const delete_ingredient = require('../models/Api/DELETE/delete_ingredient');
 const delete_order = require('../models/Api/DELETE/delete_order');

f188503b5de8fea249c54d398ab705a860d56b42
Hsutra <96309579+Hsutra@users.noreply.github.com>
2024-03-21 15:57:00 +0300

    + schedule

--- desktop/waiter/main.py
+++ desktop/waiter/main.py
@@ -1,10 +1,13 @@
 import sys
 import os
 import platform
+import threading
+import time
 
+import schedule
 from PySide6.QtWidgets import QMainWindow
 from PySide6 import QtWidgets
-
+from datetime import datetime
 from modules.addview import Ui_Dialog
 from modules.addview2 import Ui_Dialog2
 from modules.LoginWindow import Ui_Dialog3
@@ -32,15 +35,33 @@ class MainWindow(QMainWindow):
         global widgets
         widgets = self.ui
 
+        # SET API WORK
+        # ///////////////////////////////////////////////////////////////
         self.api = ApiConnect()
 
+        # Запуск обновления данных каждые 10 минут
+        schedule.every(3).hours.do(self.update_json_files)
+        schedule.run_pending()
+        time.sleep(1)
+
         # USE CUSTOM TITLE BAR | USE AS "False" FOR MAC OR LINUX
+        # ///////////////////////////////////////////////////////////////
         Settings.ENABLE_CUSTOM_TITLE_BAR = True
 
+        # APP NAME
+        # ///////////////////////////////////////////////////////////////
+        title = "SOLIDSIGN - для официантов"
+        description = "SOLIDSIGN APP - Theme with colors based on Dracula for Python."
+        # APPLY TEXTS
+        # self.setWindowTitle(title)
+        # widgets.titleRightInfo.setText(description)
+
         # TOGGLE MENU
+        # ///////////////////////////////////////////////////////////////
         widgets.toggleButton.clicked.connect(lambda: UIFunctions.toggleMenu(self, True))
 
         # SET UI DEFINITIONS
+        # ///////////////////////////////////////////////////////////////
         UIFunctions.uiDefinitions(self)
 
         # LEFT MENUS
@@ -48,15 +69,22 @@ class MainWindow(QMainWindow):
         widgets.btn_widgets.clicked.connect(self.buttonClick)
         widgets.btn_new.clicked.connect(self.buttonClick)
 
+        # EXTRA LEFT BOX
+        # def openCloseLeftBox():
+        #    UIFunctions.toggleLeftBox(self, True)
 
+        # widgets.toggleLeftBox.clicked.connect(openCloseLeftBox)
+        # widgets.extraCloseColumnBtn.clicked.connect(openCloseLeftBox)
 
-        # Открываем файл ordernum и читаем из него текст
-        with open('ordernum', 'r') as file:
-            num = file.read()
-        current_text = widgets.label_2.text()
-        combined_text = f"{current_text} {num}"
-        widgets.label_2.setText(combined_text)
+        # EXTRA RIGHT BOX
+        # def openCloseRightBox():
+        #    UIFunctions.toggleRightBox(self, True)
 
+        # widgets.settingsTopBtn.clicked.connect(openCloseRightBox)
+
+        # SHOW APP
+        # ///////////////////////////////////////////////////////////////
+        # self.show()
         # SET CUSTOM THEME
         # ///////////////////////////////////////////////////////////////
         self.themeFile = "themes/py_dracula_dark.qss"
@@ -64,7 +92,7 @@ class MainWindow(QMainWindow):
         UIFunctions.theme(self, self.themeFile, True)
 
         widgets.label.setStyleSheet("image: url(images/images/logo.png)")
-        #widgets.toggleLeftBox.setStyleSheet("background-image: url(images/icons/moon.png)")
+        # widgets.toggleLeftBox.setStyleSheet("background-image: url(images/icons/moon.png)")
 
         widgets.settingsTopBtn.clicked.connect(lambda: UIFunctions.toggle_theme(self))
 
@@ -94,23 +122,22 @@ class MainWindow(QMainWindow):
         widgets.addrow_btn.clicked.connect(lambda: UIFunctions.generate_new_row(self))
         widgets.delrow_btn.clicked.connect(lambda: UIFunctions.delete_row(self))
         widgets.clearbtn.clicked.connect(lambda: UIFunctions.clear_table(self))
-        widgets.utvrbtn.clicked.connect(lambda: UIFunctions.commit(self, widgets.tableWidget_2))  # Здесь могла быть ваша функция:)
+        widgets.utvrbtn.clicked.connect(
+            lambda: UIFunctions.commit(self, widgets.tableWidget_2))  # Здесь могла быть ваша функция:)
 
         # 2 ВКЛАДКА
         widgets.pushButton_3.clicked.connect(lambda: UIFunctions.delete_row_content(self, widgets.tableWidget))
-        widgets.pushButton_2.clicked.connect(self.open_new_window)
+        widgets.pushButton_2.clicked.connect(lambda: UIFunctions.open_new_window(self))
 
         # 3 ВКЛАДКА
         widgets.pushButton_9.clicked.connect(lambda: UIFunctions.delete_row_content(self, widgets.tableWidget_3))
-        widgets.pushButton_8.clicked.connect(self.open_new_window2)
+        widgets.pushButton_8.clicked.connect(lambda: UIFunctions.open_new_window2(self))
 
         self.ui_dialog.addtransbtn.clicked.connect(self.update_second_table)
         self.ui_dialog2.addtransbtn.clicked.connect(self.update_third_table)
 
         tab1_column_widths = [80, 200, 200, 200, 200, 200]
         UIFunctions.set_column_widths(self, widgets.tableWidget, tab1_column_widths)
-
-
 
         tab2_column_widths = [80, 200, 200, 200, 200, 200, 200, 200]
         UIFunctions.set_column_widths(self, widgets.tableWidget_3, tab2_column_widths)
@@ -123,12 +150,13 @@ class MainWindow(QMainWindow):
         self.connect_sorting_function(widgets.tableWidget_2)
         self.connect_sorting_function(widgets.tableWidget_3)
 
-
+        # SET HACKS
+        # AppFunctions.setThemeHack(self)
 
         # SET HOME PAGE AND SELECT MENU
+        # ///////////////////////////////////////////////////////////////
         widgets.stackedWidget.setCurrentWidget(widgets.home)
         widgets.btn_home.setStyleSheet(UIFunctions.selectMenu(widgets.btn_home.styleSheet()))
-
 
     # BUTTONS CLICK
     # Post here your functions for clicked buttons
@@ -156,6 +184,12 @@ class MainWindow(QMainWindow):
             UIFunctions.resetStyle(self, btnName)  # RESET ANOTHERS BUTTONS SELECTED
             btn.setStyleSheet(UIFunctions.selectMenu(btn.styleSheet()))  # SELECT MENU
 
+        if btnName == "btn_save":
+            print("Save BTN clicked!")
+
+        # PRINT BTN NAME
+        print(f'Button "{btnName}" pressed!')
+
     # RESIZE EVENTS
     # ///////////////////////////////////////////////////////////////
     def resizeEvent(self, event):
@@ -175,48 +209,42 @@ class MainWindow(QMainWindow):
             print('Mouse click: RIGHT CLICK')
 
     def open_new_window(self):
-        selected_row = self.ui.tableWidget.currentRow()
-        if selected_row >= 0:  # Проверяем, что строка действительно выбрана
-            # Получаем содержимое каждой ячейки выбранной строки
-            line = self.ui.tableWidget.item(selected_row, 1).text()
-            datetime1 = self.ui.tableWidget.item(selected_row, 2).text()
-            datetime2 = self.ui.tableWidget.item(selected_row, 3).text()
-            line2 = self.ui.tableWidget.item(selected_row, 4).text()
-            combBox = self.ui.tableWidget.item(selected_row, 5).text()
-
-            # Отображаем данные выбранной строки в диалоговом окне перед его открытием
-            self.ui_dialog.lineEdit.setText(line)
-            self.ui_dialog.dateTimeEdit.setDateTime(QDateTime.fromString(datetime1, "yyyy-MM-dd hh:mm:ss"))
-            self.ui_dialog.dateTimeEdit_2.setDateTime(QDateTime.fromString(datetime2, "yyyy-MM-dd hh:mm:ss"))
-            self.ui_dialog.lineEdit_2.setText(line2)
-            self.ui_dialog.comboBox.setCurrentText(combBox)
         self.new_window.show()
 
     def open_new_window2(self):
-        selected_row = self.ui.tableWidget_3.currentRow()
-        if selected_row >= 0:  # Проверяем, что строка действительно выбрана
-            # Получаем содержимое каждой ячейки выбранной строки
-            line = self.ui.tableWidget_3.item(selected_row, 1).text()
-            datetime1 = self.ui.tableWidget_3.item(selected_row, 2).text()
-            datetime2 = self.ui.tableWidget_3.item(selected_row, 3).text()
-            line2 = self.ui.tableWidget_3.item(selected_row, 4).text()
-            line3 = self.ui.tableWidget_3.item(selected_row, 5).text()
-            line4 = self.ui.tableWidget_3.item(selected_row, 6).text()
-
-            # Отображаем данные выбранной строки в диалоговом окне перед его открытием
-            self.ui_dialog2.lineEdit.setText(line)
-            self.ui_dialog2.dateTimeEdit.setDateTime(QDateTime.fromString(datetime1, "yyyy-MM-dd hh:mm:ss"))
-            self.ui_dialog2.dateTimeEdit_2.setDateTime(QDateTime.fromString(datetime2, "yyyy-MM-dd hh:mm:ss"))
-            self.ui_dialog2.lineEdit_2.setText(line2)
-            self.ui_dialog2.lineEdit_3.setText(line3)
-            self.ui_dialog2.lineEdit_4.setText(line4)
-
         self.new_window2.show()
+
+    def update_json_files(self):
+        endpoints = ['get_order_history', 'get_all_booked_tables']  # список всех эндпоинтов, которые нужно обновить
+
+        for endpoint in endpoints:
+            data = self.api.get_data(endpoint)
+            if data:
+                with open(f"../waiter/modules/api/jsons/{endpoint}.json", "w") as file:
+                    json.dump(data, file)
 
     def login(self):
         username = self.ui_dialog3.lineEdit.text()
         password = self.ui_dialog3.lineEdit_2.text()
+
+        # self.curUser = User.authorization(username, password, self.api)
+        #
+        # if self.curUser is not None:
+        #     pass
+
         if username == "" and password == "":
+
+            self.update_json_files()
+            # Создаем и запускаем потоки для заполнения таблиц
+            order_thread = threading.Thread(target=self.fill_table_widget, args=(self.ui.tableWidget,))
+            table_booking_thread = threading.Thread(target=self.fill_table_widget, args=(self.ui.tableWidget_3,))
+            order_thread.start()
+            table_booking_thread.start()
+
+            # Ожидаем завершения потоков
+            order_thread.join()
+            table_booking_thread.join()
+
             self.new_window3.close()
             self.show()
         else:
@@ -270,7 +298,6 @@ class MainWindow(QMainWindow):
             selected_row = self.ui.tableWidget.currentRow()
 
             if selected_row >= 0:  # Проверяем, что строка действительно выбрана
-
                 # Устанавливаем значения в каждом столбце выбранной строки
                 self.ui.tableWidget.setItem(selected_row, 0,
                                             QTableWidgetItem(str(selected_row + 1)))  # автоинкрементный id
@@ -336,7 +363,7 @@ class MainWindow(QMainWindow):
             if selected_row >= 0:  # Проверяем, что строка действительно выбрана
                 # Устанавливаем значения в каждом столбце выбранной строки
                 self.ui.tableWidget_3.setItem(selected_row, 0,
-                                            QTableWidgetItem(str(selected_row + 1)))  # автоинкрементный id
+                                              QTableWidgetItem(str(selected_row + 1)))  # автоинкрементный id
                 self.ui.tableWidget_3.setItem(selected_row, 1, QTableWidgetItem(combBox))
                 self.ui.tableWidget_3.setItem(selected_row, 2, QTableWidgetItem(line1))
                 self.ui.tableWidget_3.setItem(selected_row, 3, QTableWidgetItem(datetime1))
@@ -347,12 +374,12 @@ class MainWindow(QMainWindow):
 
                 # TODO: Добавить валидацию
                 data = {
-                  "id_table": selected_row+1,
-                  "id_worker": line1,
-                  "phone_client": line2,
-                  "order_time": datetime1,
-                  "desired_booking_time": datetime2,
-                  "booking_interval": line3
+                    "id_table": selected_row + 1,
+                    "id_worker": line1,
+                    "phone_client": line2,
+                    "order_time": datetime1,
+                    "desired_booking_time": datetime2,
+                    "booking_interval": line3
                 }
                 print(data)
 
@@ -362,39 +389,70 @@ class MainWindow(QMainWindow):
             pass
 
     def fill_table_widget(self, tableWidget):
+        if tableWidget.objectName() == 'tableWidget':
+            field_mapping = {
+                "№": "id_order",
+                "Официант": "id_worker",
+                "Дата выдачи": "giving_date",
+                "Дата формирования": "formation_date",
+                "Блюда": "dishes",
+                "Статус": "status"
+            }
+            endpoint = 'get_order_history'
+            date_keys = ['formation_date', 'giving_date']
+        elif tableWidget.objectName() == 'tableWidget_3':
+            field_mapping = {
+                "№": "table_id",
+                "Официант": "worker_id",
+                "Дата заказа": "booking_date",
+                "Желаема дата": "desired_date",
+                "Номер телефона": "client_number",
+                "Интервал брони": "booking_interval"
+            }
+            endpoint = 'get_all_booked_tables'
+            date_keys = ['booking_date', 'desired_date']
+        else:
+            return  # Если таблица не соответствует ни одному известному типу данных, выходим из функции
 
-        data = [
-            (1, 2, 3, 4, 5, 6),
-            (7, 8, 9, 10, 11, 12),
-            (1, 2, 3, 4, 5, 6),
-        ]
+        # Загружаем данные из JSON файла
+        with open(f"../waiter/modules/api/jsons/{endpoint}.json", "r") as file:
+            json_data = json.load(file)
+
+        data = json_data['data'][0]['view_order_history'] if endpoint == 'get_order_history' else json_data['data']
 
         tableWidget.setRowCount(len(data))
-        tableWidget.setColumnCount(len(data[0]))
+        tableWidget.setColumnCount(len(field_mapping))
+
+        headers = list(field_mapping.keys())
 
         for row_idx, row_data in enumerate(data):
-            for col_idx, cell_data in enumerate(row_data):
-                item = QTableWidgetItem(str(cell_data))
+            for col_idx, header in enumerate(headers):
+                key = field_mapping[header]
+                if key == "":
+                    continue
+                if endpoint == 'get_all_booked_tables':
+                    if key in date_keys:
+                        date_obj = datetime.strptime(row_data[key], "%Y-%m-%dT%H:%M:%S.%fZ")
+                        formatted_date = date_obj.strftime("%d.%m.%Y %H:%M:%S")
+                        item = QTableWidgetItem(formatted_date)
+                    elif key == 'booking_interval':
+                        item = QTableWidgetItem(str(row_data[key]['hours']))
+                    else:
+                        item = QTableWidgetItem(str(row_data[key]))
+                elif endpoint == 'get_order_history':
+                    if key in date_keys:
+                        date_obj = datetime.fromisoformat(row_data[key])
+                        formatted_date = date_obj.strftime("%d.%m.%Y %H:%M:%S")
+                        item = QTableWidgetItem(formatted_date)
+                    elif key == 'dishes':
+                        dishes_str = ', '.join([f'{dish}: {qty}' for dish, qty in row_data[key].items()])
+                        item = QTableWidgetItem(dishes_str)
+                    else:
+                        item = QTableWidgetItem(str(row_data[key]))
+                else:
+                    continue
                 tableWidget.setItem(row_idx, col_idx, item)
 
-    def commit(self, table):
-        with open('ordernum', 'r') as file:
-            order_num = file.read() # номер заказа (после нажатия кнопки наверное стоит его увеличить на 1)
-        waiter_id = self.ui.lineEdit.text() # id официанта
-        print(waiter_id)
-        data_dict = {}
-        column_count = table.columnCount()
-
-        for row_index in range(table.rowCount()):
-            item_id = int(table.item(row_index, 0).text())
-            name = table.item(row_index, 1).text()
-            count = int(table.item(row_index, 2).text())
-            comment = table.item(row_index, 3).text()
-
-            data_dict[str(item_id)] = {"name": name, "count": count, "comment": comment}
-
-        json_data = {"data": data_dict}
-        print("Data saved:", json_data)
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)

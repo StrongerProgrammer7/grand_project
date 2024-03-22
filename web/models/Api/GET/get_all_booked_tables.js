9f5123fcfb4cef710e236273187bce646f690410
Abdyukov Zufar <swat55551@gmail.com>
2024-03-22 22:39:55 +0300

    Added field interval - get all booked tables

--- web/models/Api/GET/get_all_booked_tables.js
+++ web/models/Api/GET/get_all_booked_tables.js
@@ -2,13 +2,30 @@ const DataApi = require("../../../HandleAPI/DataApi");
 const errorHandler = require('../errorHandler');
 const db = require('../../db');
 
+
+const addFieldInterval = (data) =>
+{
+    for (let i = 0; i < data.length; i++)
+    {
+        let elem = data[i];
+        const start_date = new Date(elem.start_date).getHours();
+        const end_date = new Date(elem.end_date).getHours();
+        const interval = end_date - start_date;
+        elem['interval'] = interval;
+        data[i] = elem;
+    }
+    return data;
+}
+
 const get_all_booked_tables = async (req, res, next) =>
 {
 
     db.query('SELECT * FROM view_all_booked_tables();')
         .then((data) =>
         {
-            return next(DataApi.success(data.rows, "Success!"));
+            const result = data.rows;
+
+            return next(DataApi.success(addFieldInterval(result), "Success!"));
         })
         .catch(err =>
         {

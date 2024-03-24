b23bd4cf090ac4be34ba15ee3c543b1fd4f8c40c
Memeladon <bingorussian@mail.ru>
2024-03-24 13:57:40 +0300

    Временно без ввода

--- desktop/waiter/modules/api/connection.py
+++ desktop/waiter/modules/api/connection.py
@@ -1,5 +1,6 @@
 import json
 import os
+
 import urllib3
 import requests
 import socketio
@@ -9,12 +10,43 @@ urllib3.disable_warnings()
 
 
 class ApiConnect:
-    api_url = f'https://{DOMEN}/api'
-    # Создаем сокет-клиент и подключаемся
-    sio = socketio.Client()
+    def __init__(self, ssl_cert):
+        self.api_url = f'https://{DOMEN}/api'
+        self.sio = socketio.Client(ssl_verify=ssl_cert)
+        self.sio.on('connect', self.connect)
+        self.sio.on('disconnect', self.disconnect)
+        self.sio.on('message', self.on_message)
 
-    sio.connect('http://localhost:8080')
-    # sio.connect(api_url)
+    def connect_to_server(self):
+        # Подключаемся к серверу
+        self.sio.connect('http://localhost:8080',
+                         auth={"token": "0x0324234", "name": "waiter", "idWaiter": 1})
+
+    def connect(self):
+        print("Connected to the server")
+        self.send_initial_data()
+
+    def disconnect(self):
+        print("Disconnected from the server")
+
+    def on_message(self, data):
+        print('Message from server:', data)
+
+    def send_initial_data(self):
+        auth = {"token": "0x0324234", "name": "waiter", "idWaiter": 1}
+        self.sio.emit('auth', auth)
+
+    def connect_to_server(self):
+        self.sio.connect(self.api_url)
+        # self.sio.connect('http://localhost:8080')
+
+    # WEBSOCKET MOMENT
+    def send_message(self, data):
+        # Создаем JSON-сообщение
+        message = {"text": "Сообщение отправлено на сервер"}
+        # Отправляем сообщение через сокет
+        self.sio.emit('message', data)
+        print("Message sent:", message)
 
     def get_data(self, endpoint: str):
         if endpoint:
@@ -54,25 +86,3 @@ class ApiConnect:
             if response.status_code == 201:
                 return response.json()
         return None
-
-    # WEBSOCKET MOMENT
-    def send_message(self, data):
-        # Создаем JSON-сообщение
-        message = {"text": "Сообщение отправлено на сервер"}
-        # Отправляем сообщение через сокет
-        self.sio.emit('message', data)
-        print("Message sent:", message)
-
-    @sio.event
-    def connect(self):
-        print("Connected to the server")
-
-    @sio.event
-    def disconnect(self):
-        print("Disconnected from the server")
-
-    @sio.on('message')
-    def on_message(self, data):
-        print('Message from server:', data)
-
-
